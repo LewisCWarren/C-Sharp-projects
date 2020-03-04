8652619371a09bc57edf416e5c76ba54eafe0320
Lewis Warren <57116148+LewisCWarren@users.noreply.github.com>
2020-03-04 14:10:38 -0800

    Update AdminController.cs

--- CarInsuranceProject/CarInsuranceProject/Controllers/AdminController.cs
+++ CarInsuranceProject/CarInsuranceProject/Controllers/AdminController.cs
@@ -13,11 +13,14 @@ namespace CarInsuranceProject.Controllers
         // GET: Admin
         public ActionResult Index()
         {
+        //Calling database created using Entity Framework database first
             using (CarInsuranceEntities db = new CarInsuranceEntities())
             {
+                //Creating a list<> of client records, and instanting an empty list for the view model
                 var clients = db.ClientRecords.ToList();
                 List<ClientRecordVm> clientRecordVm = new List<ClientRecordVm>();
-
+                    
+                //Passing relevant data from the database record to the view model "clientVm" which will be displayed in the view    
                 foreach (var client in clients)
                 {
                     var clientVm = new ClientRecordVm();
@@ -26,7 +29,7 @@ namespace CarInsuranceProject.Controllers
                     clientVm.LastName = client.LastName;
                     clientVm.Quote = Convert.ToInt32(client.Quote);
                     clientVm.EmailAddress = client.EmailAddress;
-
+                    
                     clientRecordVm.Add(clientVm);
                 }
                 return View(clientRecordVm);
